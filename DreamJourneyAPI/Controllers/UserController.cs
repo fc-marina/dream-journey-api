087ca07abbd49e9327749498861071ab92bb7849
Marina Faria <fc.marina@outlook.com>
2023-04-21 22:37:49 -0300

    first commit

---
+++ DreamJourneyAPI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using DreamJourneyAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamJourneyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> GetAllUsers() {
+            return Ok();
+        }
+    }
+}
---
+++ DreamJourneyAPI/Models/DreamModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DreamJourneyAPI.Models
+{
+    public class DreamModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int LifeArea { get; set; }
+        public int Status { get; set; }
+
+    }
+}
