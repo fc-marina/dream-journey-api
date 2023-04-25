cb63f4df973b91ac52904c07e148967cd1d601d5
Marina Faria <fc.marina@outlook.com>
2023-04-25 16:55:20 -0300

    Inclusão de teste unitário

--- DreamJourneyAPI.Tests/Repositories/DreamRepositoryTest.cs
+++ DreamJourneyAPI.Tests/Repositories/DreamRepositoryTest.cs
@@ -8,68 +8,36 @@ namespace DreamJourneyAPI.Tests.Repositories
 {
     public class DreamRepositoryTest
     {
-        //private Mock<ILogger<DreamJourneydbcontext>>? _mock;
-        //private dreamjourneydbcontext? _context;
-        //private dreamrepository _dreamrepository;
+        public DreamRepositoryTest()
+        {
 
-        //public Dreamrepositorytest()
-        //{
-        //    _mock = new Mock<ILogger<dreamjourneydbcontext>>();
-        //    createdreamjourneydbcontext();
+        }
 
-        //}
+        [Fact]
+        public async Task GetAll_ShouldReturnListOfDreamModels()
+        {
+            //var fixture = new Fixture();
+            //List<DreamModel> dreamModelss = fixture.CreateMany<DreamModel>(3).ToList();
 
-        //private void CreateDreamJourneyDbContext()
-        //{
-        //    var options = new DbContextOptionsBuilder<DreamJourneyDbContext>().Options;
-        //    _context = (DreamJourneyDbContext)Activator.CreateInstance(typeof(DreamJourneyDbContext), new object[] { options });
-        //    _context.Database.EnsureDeleted();
-        //    _context.Database.EnsureCreated();
-        //}
+            var dreamModels = new List<DreamModel>
+            {
+            new DreamModel { Id = 1, Name = "Dream1", Description = "User1" },
+            new DreamModel { Id = 2, Name = "Dream2", Description = "User2" }
+            }.AsQueryable();
 
-        //[Fact]
-        //public async Task GetAll_ShouldReturnListOfDreamModels()
-        //{
-        //    var dream1 = new DreamModel { Id = 1, Name = "Dream 1", UserId = 1, Description = "Description", LifeArea= 0, Status= 0};
-        //    var dream2 = new DreamModel { Id = 1, Name = "Dream 1", UserId = 1, Description = "Description", LifeArea = 0, Status = 0 };
-        //    _context.DreamModel.AddRange(dream1, dream2);
-        //    await _context.SaveChangesAsync();
-        //    _dreamRepository = new DreamRepository(_context);
-        //    var result = await _dreamRepository.GetAll(0, 2);
+            var dbSetMock = new Mock<DbSet<DreamModel>>();
+            dbSetMock.As<IQueryable<DreamModel>>().Setup(m => m.Provider).Returns(dreamModels.Provider);
+            dbSetMock.As<IQueryable<DreamModel>>().Setup(m => m.Expression).Returns(dreamModels.Expression);
+            dbSetMock.As<IQueryable<DreamModel>>().Setup(m => m.ElementType).Returns(dreamModels.ElementType);
+            dbSetMock.As<IQueryable<DreamModel>>().Setup(m => m.GetEnumerator()).Returns(dreamModels.GetEnumerator());
 
-        //    Assert.Equal(2, result.Count);
+            var dbContextMock = new Mock<DreamJourneyDbContext>();
+            dbContextMock.Setup(dbContext => dbContext.Set<DreamModel>()).Returns(dbSetMock.Object);
 
-        //}
-
-        //private DreamRepository _dreamRepository;
-        //private DreamJourneyDbContext _dbContext;
-
-        //public DreamRepositoryTest()
-        //{
-        //    var options = new DbContextOptionsBuilder<DreamJourneyDbContext>()
-        //        .UseInMemoryDatabase(databaseName: "TestDb")
-        //        .Options;
-        //    _dbContext = new DreamJourneyDbContext(options);
-
-        //    _dreamRepository = new DreamRepository(_dbContext);
-        //}
-
-        //[Fact]
-        //public async Task GetAll_ShouldReturList()
-        //{
-
-        //    var dream1 = new DreamModel { Id = 1, Name = "Dream 1", Description = "x", LifeArea = 0, Status = 0 };
-        //    var dream2 = new DreamModel { Id = 2, Name = "Dream 2", Description = "y", LifeArea = 0, Status = 0 };
-        //    _dbContext.DreamModel.AddRange(dream1, dream2);
-
-        //    await _dbContext.SaveChangesAsync();
-
-        //    var result = await _dreamRepository.GetAll(0, 2);
-
-        //    Assert.Equal(2, result.Count);
-
-        //    Assert.Equal("Dream 1", result[0].Name);
-        //    Assert.Equal("Dream 2", result[1].Name);
-        //}
+            var dreamRepository = new DreamRepository(dbContextMock.Object);
+            var result = await dreamRepository.GetAll(0, 2);
+            Assert.Equal(dreamModels.ToList(), result);
+        }
     }
 }
+
